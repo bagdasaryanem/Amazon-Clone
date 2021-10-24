c5e96aa385afce7da555db41a4d66492f7390e89
bagdasaryanem <bagdasaryanem@gmail.com>
2021-10-24 15:47:22 +0400

    feat: added graphql and product entity

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/product/product.entity.ts
@@ -0,0 +1,13 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export class Product {
+  @Field(() => Int)
+  id: number;
+
+  @Field()
+  name: string;
+
+  @Field()
+  price: number;
+}
